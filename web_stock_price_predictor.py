19bd66412fea0038e0a355d59138edd6ab2818e3
Dharmendra Raj <113377567+Dharmendra123rj@users.noreply.github.com>
2024-12-01 22:16:17 +0530

    Updated Website

--- web_stock_price_predictor.py
+++ web_stock_price_predictor.py
@@ -47,37 +47,37 @@ st.pyplot(plot_graph((15,6), google_data['MA_for_100_days'],google_data,0))
 st.subheader('Original Close Price and MA for 100 days and MA for 250 days')
 st.pyplot(plot_graph((15,6), google_data['MA_for_100_days'],google_data,1,google_data['MA_for_250_days']))
 
-from sklearn.preprocessing import MinMaxScaler
+# from sklearn.preprocessing import MinMaxScaler
 
-scaler = MinMaxScaler(feature_range=(0,1))
-scaled_data = scaler.fit_transform(x_test[['Close']])
+# scaler = MinMaxScaler(feature_range=(0,1))
+# scaled_data = scaler.fit_transform(x_test[['Close']])
 
-x_data = []
-y_data = []
+# x_data = []
+# y_data = []
 
-for i in range(100,len(scaled_data)):
-    x_data.append(scaled_data[i-100:i])
-    y_data.append(scaled_data[i])
+# for i in range(100,len(scaled_data)):
+#     x_data.append(scaled_data[i-100:i])
+#     y_data.append(scaled_data[i])
 
-x_data, y_data = np.array(x_data), np.array(y_data)
+# x_data, y_data = np.array(x_data), np.array(y_data)
 
-predictions = model.predict(x_data)
+# predictions = model.predict(x_data)
 
-inv_pre = scaler.inverse_transform(predictions)
-inv_y_test = scaler.inverse_transform(y_data)
+# inv_pre = scaler.inverse_transform(predictions)
+# inv_y_test = scaler.inverse_transform(y_data)
 
-ploting_data = pd.DataFrame(
- {
-  'original_test_data': inv_y_test.reshape(-1),
-    'predictions': inv_pre.reshape(-1)
- } ,
-    index = google_data.index[splitting_len+100:]
-)
-st.subheader("Original values vs Predicted values")
-st.write(ploting_data)
+# ploting_data = pd.DataFrame(
+#  {
+#   'original_test_data': inv_y_test.reshape(-1),
+#     'predictions': inv_pre.reshape(-1)
+#  } ,
+#     index = google_data.index[splitting_len+100:]
+# )
+# st.subheader("Original values vs Predicted values")
+# st.write(ploting_data)
 
-st.subheader('Original Close Price vs Predicted Close price')
-fig = plt.figure(figsize=(15,6))
-plt.plot(pd.concat([google_data.Close[:splitting_len+100],ploting_data], axis=0))
-plt.legend(["Data- not used", "Original Test data", "Predicted Test data"])
-st.pyplot(fig)
+# st.subheader('Original Close Price vs Predicted Close price')
+# fig = plt.figure(figsize=(15,6))
+# plt.plot(pd.concat([google_data.Close[:splitting_len+100],ploting_data], axis=0))
+# plt.legend(["Data- not used", "Original Test data", "Predicted Test data"])
+# st.pyplot(fig)
